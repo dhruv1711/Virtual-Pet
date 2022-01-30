7d354a890611eef74ee18cec1b060f087f83e612
Dhruv Iyer <63696398+dhruv1711@users.noreply.github.com>
2022-01-31 03:48:44 +0900

    Initial commit

---
+++ Assets/Syazmedia Tools/AnimationPlayer/AnimationPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimationPlayer : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Animator GetTargetAnimator()
+    {
+        if(animator == null)
+        {
+            animator = gameObject.GetComponent<Animator>();
+            return animator;
+        }
+        else
+        {
+            return animator;
+        }
+        
+    }
+
+    public void PlayAnimation(string animationName)
+    {
+        if(animationName != "")
+        {
+            animator.StopPlayback();
+            animator.Play(animationName);
+        }
+
+        
+    }
+
+
+}
